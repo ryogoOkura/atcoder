a60da8348188aebebcdb5eedbcd426002d029880
Ryogo Okura <nem1442so@gmail.com>
2020-07-12 01:00:06 +0900

    ac AISing2020-d

--- entrant/AISing2020/AISing2020-d.py
+++ entrant/AISing2020/AISing2020-d.py
@@ -1,27 +1,41 @@
+## 最初のmodをどうにか高速にやりたい → modの性質を使う
 ## xiのpopcount（1の数）はxのpopcount+1または-1
 n=int(input())
-x=input()
-f={0:0,1:1,2:1,3:2}
-# for i in range(2,n):
-#     f[2**i]=1
-# print(f)
-for i in range(len(x)):
-    if x[i]=='1':
-        x_changed=x[:i]+'0'+x[i+1:]
+x_str=input()
+x_popcount=x_str.count('1')
+dp=[0]*(x_popcount+1)
+for i in range(1,x_popcount+1):
+    popcount=format(i,'b').count('1')
+    dp[i]=dp[i%popcount]+1
+# print(dp)
+# x=int(x_str,2)
+if x_popcount>1:
+    pows_minus=[pow(2,i,(x_popcount-1)) for i in range(n)]
+    # x_minus=x%(x_popcount-1)
+    x_minus=0
+    for i in range(n):
+        if x_str[i]=='1':
+            x_minus+=pows_minus[n-1-i]
+pows_plus=[pow(2,i,(x_popcount+1)) for i in range(n)]
+# x_plus=x%(x_popcount+1)
+x_plus=0
+for i in range(n):
+    if x_str[i]=='1':
+        x_plus+=pows_plus[n-1-i]
+
+for i in range(len(x_str)):
+    ## xi=xi mod popcount(x)+-1
+    xi=-1
+    if x_str[i]=='1':
+        if x_popcount>1: ## else: xi=-1
+            xi=(x_minus-pows_minus[n-1-i])%(x_popcount-1)
     else:
-        x_changed=x[:i]+'1'+x[i+1:]
-    num_changed=int(x_changed,2)
-    add=[]
-    tmp=num_changed
-    while tmp not in f:
-        popcount=format(tmp,'b').count('1')
-        add.append(tmp)
-        tmp%=popcount
-    while len(add)>0:
-        cnt=f[tmp]
-        tmp=add.pop()
-        f[tmp]=cnt+1
-    print(f[num_changed])
+        xi=(x_plus+pows_plus[n-1-i])%(x_popcount+1)
+    if xi==-1:
+        print(0)
+    else:
+        print(dp[xi]+1)
+
 
 ## TLE 考えてみたら下のやつと同じオーダーだった
 # n=int(input())
