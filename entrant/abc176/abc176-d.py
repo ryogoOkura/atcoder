4ab0a127154779986579f0e906e8aa18c5b58d94
Ryogo Okura <nem1442so@gmail.com>
2020-08-27 16:25:21 +0900

    ac abc176-d,e

--- entrant/abc176/abc176-d.py
+++ entrant/abc176/abc176-d.py
@@ -1,34 +1,97 @@
 from collections import deque
 import math
 
-def solve2(s_flag,h,w,goal_h,goal_w,que):
-    while que:
-        ch,cw=que.popleft()
-        now=s_flag[ch][cw]
-        for dh,dw in [(1,0),(0,1),(0,-1),(-1,0)]:
-            if ch+dh<0 or h-1<ch+dh:
-                continue
-            if cw+dw<0 or w-1<cw+dw:
-                continue
-            dist=s_flag[ch+dh][cw+dw]
-            if dist=='#': ## wall
-                jump=[(dh*2,dw*2)]
-                for tmp in range(1,3):
-                    jump.append((dh*2+dw*tmp,dw*2+dh*tmp))
-                    jump.append((dh*2-dw*tmp,dw*2-dh*tmp))
+def solve3(s_flag,h,w,goal_h,goal_w,que):
+    while True:
+        visited=set()
+        while que:
+            ch,cw=que.popleft()
+            visited.add((ch,cw))
+            ## 現在地のコスト
+            now=s_flag[ch][cw]
+            for d_h,d_w in [(1,0),(0,1),(0,-1),(-1,0)]:
+                ## 迷路の外に出ない
+                if ch+d_h<0 or h-1<ch+d_h:
+                    continue
+                if cw+d_w<0 or w-1<cw+d_w:
+                    continue
+                ## 進行方向のコスト
+                dest=s_flag[ch+d_h][cw+d_w]
+                if dest!='#': ## 道
+                    if dest>now:
+                        s_flag[ch+d_h][cw+d_w]=now
+                        que.append([ch+d_h,cw+d_w])
+        jumping=set()
+        for ch,cw in visited:
+            now=s_flag[ch][cw]
+            for d_h,d_w in [(1,0),(0,1),(0,-1),(-1,0)]:
+                jump=[  (d_h+d_w,d_w+d_h),
+                        (d_h-d_w,d_w-d_h),
+                        (d_h*2,d_w*2),
+                        (d_h*2+d_w,d_w*2+d_h),
+                        (d_h*2-d_w,d_w*2-d_h),
+                        (d_h*2+d_w*2,d_w*2+d_h*2),
+                        (d_h*2-d_w*2,d_w*2-d_h*2)]
                 for jump_h,jump_w in jump:
                     if ch+jump_h<0 or h-1<ch+jump_h:
                         continue
                     if cw+jump_w<0 or w-1<cw+jump_w:
                         continue
-                    dist_jump=s_flag[ch+jump_h][cw+jump_w]
-                    if dist_jump!='#' and dist_jump>now+1:
+                    jumping.add((ch+jump_h,cw+jump_w))
+        if len(jumping)==0:
+            break
+        for d_jump_h,d_jump_w in jumping:
+            ## ワープ先のコスト
+            dest_jump=s_flag[d_jump_h][d_jump_w]
+            if dest_jump!='#' and dest_jump>now+1:
+                s_flag[d_jump_h][d_jump_w]=now+1
+                que.append([d_jump_h,d_jump_w])
+
+    # for i in range(h):
+    #     print(s_flag[i])
+    return s_flag[goal_h][goal_w]
+
+
+def solve2(s_flag,h,w,goal_h,goal_w,que):
+    while que:
+        ch,cw=que.popleft()
+        ## 現在地のコスト
+        now=s_flag[ch][cw]
+        for d_h,d_w in [(1,0),(0,1),(0,-1),(-1,0)]:
+            ## 迷路の外に出ない
+            if ch+d_h<0 or h-1<ch+d_h:
+                continue
+            if cw+d_w<0 or w-1<cw+d_w:
+                continue
+            ## 進行方向のコスト
+            dest=s_flag[ch+d_h][cw+d_w]
+            if dest=='#': ## wall
+                # jump=[(d_h*2,d_w*2)]
+                # for tmp in range(1,3):
+                #     jump.append((d_h*2+d_w*tmp,d_w*2+d_h*tmp))
+                #     jump.append((d_h*2-d_w*tmp,d_w*2-d_h*tmp))
+                jump=[  (d_h+d_w,d_w+d_h),
+                        (d_h-d_w,d_w-d_h),
+                        (d_h*2,d_w*2),
+                        (d_h*2+d_w,d_w*2+d_h),
+                        (d_h*2-d_w,d_w*2-d_h),
+                        (d_h*2+d_w*2,d_w*2+d_h*2)]
+                for jump_h,jump_w in jump:
+                    if ch+jump_h<0 or h-1<ch+jump_h:
+                        continue
+                    if cw+jump_w<0 or w-1<cw+jump_w:
+                        continue
+                    ## ワープ先のコスト
+                    dest_jump=s_flag[ch+jump_h][cw+jump_w]
+                    if dest_jump!='#' and dest_jump>now+1:
                         s_flag[ch+jump_h][cw+jump_w]=now+1
                         que.append([ch+jump_h,cw+jump_w])
-            else:
-                if dist>now:
-                    s_flag[ch+dh][cw+dw]=now
-                    que.append([ch+dh,cw+dw])
+            else: ## 道
+                if dest>now:
+                    s_flag[ch+d_h][cw+d_w]=now
+                    que.append([ch+d_h,cw+d_w])
+    # for i in range(h):
+    #     print(s_flag[i])
     return s_flag[goal_h][goal_w]
 
 def solve(s_flag,h,w,ch,cw,jump_cnt):
@@ -83,10 +146,10 @@ def main():
     s_flag=[[math.inf if s[i][j]=='.' else '#' for j in range(w)] for i in range(h)]
     s_flag[ch][cw]=0
     que=deque([[ch,cw]])
-    ans2=solve2(s_flag,h,w,dh,dw,que)
-    if ans2==math.inf:
-        ans2=-1
-    return ans2
+    ans=solve3(s_flag,h,w,dh,dw,que)
+    if ans==math.inf:
+        ans=-1
+    return ans
 
 if __name__=='__main__':
     ans=main()
--- entrant/abc176/abc176-e.py
+++ entrant/abc176/abc176-e.py
@@ -1,9 +1,9 @@
 def main():
     h,w,m=map(int,input().split())
-    target=[0]*m
+    target=set()
     for i in range(m):
         th,tw=map(int,input().split())
-        target[i]=(th-1,tw-1)
+        target.add((th-1,tw-1))
     lines=[0]*w
     rows=[0]*h
     for th,tw in target:
@@ -13,13 +13,16 @@ def main():
     max_rows=max(rows)
     max_wi=[i for i,wi in enumerate(lines) if wi==max_lines]
     max_hi=[i for i,hi in enumerate(rows) if hi==max_rows]
-    # print(max_rows,max_hi,max_lines,max_wi)
+    # print('rows\t',max_rows,max_hi,'\nlines\t',max_lines,max_wi)
     ans=max_lines+max_rows-1
+    isLoop=True
     for wi in max_wi:
-        for hi in max_hi:
-            if not (hi,wi) in target:
-                ans+=1
-                break
+        if isLoop:
+            for hi in max_hi:
+                if not (hi,wi) in target:
+                    ans+=1
+                    isLoop=False
+                    break
     return ans
 
 if __name__=='__main__':
